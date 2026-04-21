e43777febd1dc6168a6f4a4c3bded2a74ec43426
dominics4n <dominics4n@gmail.com>
2026-04-21 15:56:41 -0600

    Normalizar valores

--- backpropagation.py
+++ backpropagation.py
@@ -10,6 +10,25 @@ class Neuronas:
     error = 0
     salida = 0
 
+def MinMax(categorias):
+    MinVal = []
+    MaxMinVal = []
+    for categoria in categorias:
+        minimo = min(categoria)
+        maximo = max(categoria)
+        MinVal.append(minimo)
+        MaxMinVal.append(maximo - minimo)
+    return [MinVal, MaxMinVal]
+
+def Normalizar(categorias, min, max_min):
+    Normalizados = []
+    for i in range(len(categorias[0])):
+        datos = []
+        for j in range(len(categorias)):
+            datos.append((categorias[j][i]-min[j])/max_min[j])
+        Normalizados.append(datos)
+    return Normalizados
+
 def Crear_Red(parametros, num_N_ini, num_N_mid, num_N_fin):
     red = []
     for C_W in [[num_N_ini, parametros], [num_N_mid, num_N_ini], [num_N_fin, num_N_mid]]:
@@ -82,25 +101,43 @@ def Entrena(Red_N, Set_Entrenamiento, B, n_rondas, resultados):
     kys = kys + 1
 
 def Predecir(Red_N):
-    Solicitud = [0.2,0.2687146922,0.4974619289,1,0.5,0.5263157895,0,0.5]
+    Edad, Ingreso Mensual,Calificacion,Modalidad Escolar,Dependientes,Recurses,Nivel economico,Becas Adicionales = []
+    int(input("Ingresa " + xyString + ": "))
+    valido = False
+    while not valido:
+        try:
+            Edad = float(input("Ingresa la edad del aspirante: "))
+            if Edad < 1 or Edad > 99:
+                print("Ingresa una edad valida")
+            else:
+                valido = True
+        except ValueError:
+            print("Ingresa una edad valida")
     Prediccion = Propagar_Entradas(Red_N, Solicitud)
     print(Prediccion)
 
 # Open the CSV file for reading
 dataset = []
 outputs = []
-with open('becas_01.csv', newline='') as csvfile:
+Categorias = []
+Campos = []
+with open('becas.csv', newline='') as csvfile:
     csvreader = csv.reader(csvfile)
-    fields = next(csvreader)
+    Campos = next(csvreader)
+    for atributo in Campos:
+        Categorias.append([])
     for fila in csvreader:
-        datos = []
+        id = 0
         for campo in fila:
-            datos.append(float(campo))
-        outputs.append(datos.pop())
-        dataset.append(datos)
+            Categorias[id].append(float(campo))
+            id += 1
+    outputs = Categorias.pop()
 
+minmax = MinMax(Categorias)
+dataset = Normalizar(Categorias, minmax[0], minmax[1])
 n_inputs = len(dataset[0])
 n_outputs = 1
+
 Red = Crear_Red(n_inputs, 1, 1, n_outputs)
 Entrena(Red, dataset, 0.5, 20, outputs)
 Predecir(Red)
