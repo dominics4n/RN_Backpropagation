d4864426c0dade05b8118220d396bb635c0ca4b6
dominics4n <dominics4n@gmail.com>
2026-04-21 17:57:57 -0600

    Comentarios

--- backpropagation.py
+++ backpropagation.py
@@ -10,6 +10,7 @@ class Neuronas:
     error = 0
     salida = 0
 
+#Encuentra el valor minimo y maximo de cada categoria
 def MinMax(categorias):
     MinVal = []
     MaxMinVal = []
@@ -20,6 +21,7 @@ def MinMax(categorias):
         MaxMinVal.append(maximo - minimo)
     return [MinVal, MaxMinVal]
 
+#Normalizacion de los datos para su uso en la Red Neuronal
 def Normalizar(categorias, min, max_min):
     Normalizados = []
     for i in range(len(categorias[0])):
@@ -29,13 +31,17 @@ def Normalizar(categorias, min, max_min):
         Normalizados.append(datos)
     return Normalizados
 
+#Creacion de la Red NEuronal
 def Crear_Red(parametros, num_N_ini, num_N_mid, num_N_fin):
     red = []
+    #Por cada capa
     for C_W in [[num_N_ini, parametros], [num_N_mid, num_N_ini], [num_N_fin, num_N_mid]]:
         Capa = []
         for n in range(C_W[0]):
+            #Crea una Neurona
             Capa.append(Neuronas())
             datos = []
+            #Crea los pesos de la neurona
             for w in range(C_W[1]):
                 datos.append(random())
             Capa[n].pesos = datos
@@ -44,8 +50,10 @@ def Crear_Red(parametros, num_N_ini, num_N_mid, num_N_fin):
             
 def Activar_Neurona(Entradas, Neurona):
     S = 0
+    #Producto Escalar
     for x in range(len(Entradas)):
         S = S + Entradas[x] * Neurona.pesos[x]
+    #Funcion de Activacion
     return 1.0 / (1.0 + exp(-S))
 
 def Propagar_Entradas(Red_N, Soli_B):
@@ -56,16 +64,20 @@ def Propagar_Entradas(Red_N, Soli_B):
             fun_act = Activar_Neurona(entradas, neurona)
             Salidas.append(fun_act)
             neurona.salida = fun_act
+        #Usamos las Salidas como entradas para la siguiente Capa
         entradas = Salidas
     return Salidas
 
 def Propagar_Errores(Red_N, Resultado):
+    #Comenzamos desde la ultima capa
     for i in reversed(range(len(Red_N))):
         capa = Red_N[i]
         error_base = []
+        #Revisamos si nos encontramos en la ultima capa
         if i != len(Red_N) - 1:
             for j in range(len(capa)):
                 error = 0
+                #Sumamos los errores de la capa superior
                 for neurona in Red_N[i + 1]:
                     error = error + (neurona.pesos[j] * neurona.error)
                 error_base.append(error)
@@ -81,6 +93,7 @@ def Actualizar_Pesos(Red_N, Soli_Beca, B):
         Nuevos_Inputs = []
         for neurona in Red_N[i]:
             for j in range(len(inputs)):
+                #Ley del cuadrado minimo
                 neurona.pesos[j] = neurona.pesos[j] - B * neurona.error * inputs[j]
             Nuevos_Inputs.append(neurona.salida)
         inputs = Nuevos_Inputs
@@ -111,7 +124,6 @@ def Entrena(Red_N, Set_Entrenamiento, B, n_rondas, resultados, FrecuenciaPrint, 
             else:
                 printcount += 1
     
-
 def Predecir(Red_N, Solicitud):
     Prediccion = Propagar_Entradas(Red_N, Solicitud)
     print("La prediccion del sistema es: "+str(Prediccion[0]))
@@ -212,8 +224,6 @@ def Preguntas_Input():
 
     return [[Edad],[Ingreso],[Calificacion],[Modalidad],[Dependientes],[Recurses],[lvleconomico],[BecasPlus]]
 
-
-
 # Open the CSV file for reading
 dataset = []
 outputs = []
@@ -243,5 +253,4 @@ Red = Crear_Red(n_inputs, 2, 3, n_outputs)
 Entrena(Red, dataset, 0.5, 5000, outputs, frecuenciaprint, nivelprint)
 inputbase = Preguntas_Input()
 prediccion_Set = Normalizar(inputbase, minmax[0], minmax[1])
-
 Predecir(Red, prediccion_Set[0])
